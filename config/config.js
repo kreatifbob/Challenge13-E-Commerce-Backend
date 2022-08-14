b52119db11694ca87468f24492cad0b1d22157f8
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-08-14 21:48:17 +1000

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    _NODE_ENV: process.env.NODE_ENV,
+    _PORT: process.env.PORT,
+    _DB_USER: process.env.DB_USER,
+    _DB_PASSWORD: process.env.DB_PASSWORD,
+    _DB_NAME: process.env.DB_NAME,
+    _DB_PORT: process.env.DB_PORT,
+    _DB_HOST: process.env.DB_HOST,
+  };
+  
